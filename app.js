f2685a865994680d2fdaa57317114b2017ea71da
Makesh <rmakesh2000@gmail.com>
2023-07-27 11:16:15 +0530

    Level 1 username and password

---
+++ app.js
@@ -0,0 +1,73 @@
+//jshint esversion:6
+const express = require("express");
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+mongoose.connect("mongodb://127.0.0.1:27017/userDB", {useNewUrlParser: true});
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+const User = mongoose.model("User", userSchema);
+
+
+app.get("/", function(req, res){
+    res.render("home");
+});
+
+app.get("/register", function(req, res){
+    res.render("register");
+});
+
+app.get("/login", function(req, res){
+    res.render("login");
+});
+
+app.post("/register", function(req,res){
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+
+    newUser.save()
+    .then(function(){
+        res.render("secrets");
+    })
+    .catch(function(error){
+        console.log(error);
+    });
+});
+
+
+app.post("/login", function(req, res){
+
+    User.findOne({email:req.body.email})
+    .then(function(user){
+        if(user){
+            if(user.password === req.body.password){
+                res.render("secrets");
+            }
+        }
+    })
+    .catch(function(error){
+        console.log(error);
+    });
+});
+
+
+
+
+app.listen(3000, function(){
+    console.log("Server started on port 3000.");
+});
